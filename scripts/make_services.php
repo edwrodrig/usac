d81c31ae20c640b7c1308779eff57a2db802e54e
Edwin Rodríguez <edwrodrig@gmail.com>
2017-12-05 15:50:27 -0300

    Changes

--- scripts/make_services.php
+++ scripts/make_services.php
@@ -7,6 +7,7 @@ require_once(__DIR__ . '/../vendor/autoload.php');
   [
     'input_type' => 'get',
     'output_type' => 'json',
-    'output_dir' => __DIR__ . '/../ws'
+    'output_dir' => __DIR__ . '/../ws',
+    'source' => '../vendor/autoload.php'
   ]
 );
--- src/controller/Services.php
+++ src/controller/Services.php
@@ -1,8 +1,6 @@
 <?php
 namespace edwrodrig\usac\controller;
 
-require_once(__DIR__ . '/../../vendor/autoload.php');
-
 class Services {
 
 public $users;
