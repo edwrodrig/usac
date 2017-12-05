8684070d9bfbf1e48a216fa67cb13e3f2cad2ab1
Edwin Rodríguez <edwrodrig@gmail.com>
2017-12-05 17:30:12 -0300

    Refactor

--- scripts/make_services.php
+++ scripts/make_services.php
@@ -2,13 +2,13 @@
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
-$builder = new edwrodrig\generator_service\Builder([
-  'input_type' => 'get',
-  'output_type' => 'json',
-  'output_dir' => __DIR__ . '/../ws',
-  'source' => '../vendor/autoload.php'
-]);
-
-$builder->add(__DIR__ . '/../src/controller/Services.php');
-$builder->generate_specification();
+edwrodrig\generator_service\Utils::generate_from_source(
+  __DIR__ . '/../src/controller/Services.php',
+  [
+    'input_type' => 'get',
+    'output_type' => 'json',
+    'output_dir' => __DIR__ . '/../ws',
+    'source' => '../vendor/autoload.php'
+  ]
+);
 
